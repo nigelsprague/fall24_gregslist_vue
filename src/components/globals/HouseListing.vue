ab533f2ca22df94a3194980af565314477610c28
Nigel Sprague <spraguenigel27@gmail.com>
2024-09-18 16:26:11 -0600

    delete functional

--- src/components/globals/HouseListing.vue
+++ src/components/globals/HouseListing.vue
@@ -1,23 +1,46 @@
 <script setup>
+import { AppState } from '@/AppState';
 import { House } from '@/models/House';
-
+import { housesService } from '@/services/HousesService';
+import { logger } from '@/utils/Logger';
+import Pop from '@/utils/Pop';
+import { computed } from 'vue';
 
 const props = defineProps({
   houseProp: { type: House, required: true }
 })
+
+const account = computed(() => AppState.account)
+
+async function deleteHouse() {
+  try {
+    const wantsToDelete = await Pop.confirm(`Are you sure you want to delete this House listing?`)
+    if(!wantsToDelete) { return }
+    await housesService.deleteHouse(props.houseProp.id)
+  }
+  catch (error){
+    Pop.meow(error)
+    logger.error(error)
+  }
+}
 </script>
 
 
 <template>
   <div class="card">
-  <img :src="houseProp.imgUrl" class="card-img-top img-fluid house-img" :alt="houseProp.description">
-  <div class="card-body">
-    <h2>{{ houseProp.priceAsCurrency }}</h2>
-    <p class="fs-5 my-1">Built in <b>{{ houseProp.year }}</b></p>
-    <p class="fs-4"><b>{{ houseProp.bedrooms }}</b> bdrs | <b>{{ houseProp.bathrooms }}</b> bths | <b>{{ houseProp.levels }}</b> lvls</p>
-    <p class="card-text fs-5">{{ houseProp.description }}</p>
+    <img :src="houseProp.imgUrl" class="card-img-top img-fluid house-img" :alt="houseProp.description">
+    <div class="card-body">
+      <h2>{{ houseProp.priceAsCurrency }}</h2>
+      <p class="fs-5 my-1">Built in <b>{{ houseProp.year }}</b></p>
+      <p class="fs-4"><b>{{ houseProp.bedrooms }}</b> bdrs | <b>{{ houseProp.bathrooms }}</b> bths | <b>{{ houseProp.levels }}</b> lvls</p>
+      <p class="card-text fs-5">{{ houseProp.description }}</p>
+    </div>
+    <div class="pb-3 pe-3 text-end">
+          <button v-if="houseProp.creatorId == account?.id" @click="deleteHouse()" class="btn btn-danger" type="button">
+            Delete House
+          </button>
+        </div>
   </div>
-</div>
 </template>
 
 
--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,8 +1,15 @@
 import { api } from "./AxiosService"
 import { House } from "@/models/House"
 import { AppState } from "@/AppState"
+import { logger } from "@/utils/Logger"
 
 class HousesService{
+  async deleteHouse(houseId) {
+    const response = await api.delete(`api/houses/${houseId}`)
+    const houseIndex = AppState.houses.findIndex(house => house.id == houseId)
+    AppState.houses.splice(houseIndex, 1)
+  }
+
   async createHouse(houseData) {
     const response = await api.post('api/houses', houseData)
     const newHouse = new House(response.data)
@@ -14,7 +21,6 @@ class HousesService{
     const newHouses = response.data.map(house => new House(house))
     AppState.houses = newHouses
   }
-
 }
 
 export const housesService = new HousesService()
